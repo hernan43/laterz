3e18a5f4913d1c4eae7f2dd482a9e769b27297bb
Ramon Hernandez <ray@galactica.gateway.2wire.net>
2009-08-21 20:56:17 -0400

    Initial capfile check in. I hope it works but I'm sure this is some wonkiness.

---
+++ config/deploy.rb
@@ -0,0 +1,19 @@
+set :application, "laterz"
+# i think this is how it goes based on
+# http://github.com/guides/deploying-with-capistrano
+set :repository,  "git@github.com:railsrumble/rr09-team-162.git"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/rails/#{application}"
+set :deploy_via, :remote_cache
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+set :scm, :git
+
+role :app, "laterz.r09.railsrumble.com"
+role :web, "laterz.r09.railsrumble.com"
+role :db,  "laterz.r09.railsrumble.com", :primary => true
