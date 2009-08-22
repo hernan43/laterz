49e2054eba5004e9331272ef3fefb62303ee9a7f
Ramon Hernandez <ray@starscream.local>
2009-08-22 18:24:56 -0400

    Moved the shortcut URL to a helper.

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,13 @@
 module CategoriesHelper
+  def generate_category_shortcut_link(category=nil)
+    # <a href="javascript:void(location.href='<%= new_link_url %>?return=true&amp;url='+encodeURIComponent(location.href))"  onmouseover="window.status='';return true" onclick="return false">save this</a>
+    category_id_stub = ""
+    text = "save this"
+    
+    if category
+      category_id_stub = "&amp;category_id=#{category.id}"
+      text = "save this to #{h(category.name)}"
+    end
+    "<a href=\"javascript:void(location.href='#{new_link_url}?return=true#{category_id_stub}&amp;url='+encodeURIComponent(location.href))\" onmouseover=\"window.status='';return true\" onclick=\"return false\">#{text}</a>"
+  end
 end
