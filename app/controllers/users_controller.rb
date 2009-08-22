c45ca62d34d95ab101828c56d3be87c85b4e9521
Ramon Hernandez <ray@galactica.gateway.2wire.net>
2009-08-21 23:07:24 -0400

    Added the edit method for users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,18 @@ class UsersController < ApplicationController
       render :action => 'new'  
     end  
   end
+  
+  def edit  
+    @user = current_user  
+  end  
+
+  def update  
+    @user = current_user  
+    if @user.update_attributes(params[:user])  
+      flash[:notice] = "Successfully updated profile."  
+      redirect_to root_url  
+    else  
+      render :action => 'edit'  
+    end  
+  end
 end
